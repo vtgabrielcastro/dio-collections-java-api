f5c945f87f4181adccef2502ceed4fbc99f339e9
Victor <victorgabrielcastrosilva2015@gmail.com>
2025-04-26 20:32:48 -0300

    Ordenando Comparator e Comparable

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import list.Pesquisa.CatalogoLivros;
+import list.Ordenacao.OrdenacaoPessoa;
 
 public class Main {
 
@@ -19,6 +19,7 @@ public class Main {
 		listaTarefa.obterDescricoesTarefas();
 		
 		-------------------------------------------------------
+		
 		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
 		
 		carrinho.adicionarItem("Camisa", 20, 1);
@@ -36,7 +37,9 @@ public class Main {
 		carrinho.exibirItens();
 		
 		System.out.println("Valor total: " + carrinho.calcularValorTotal());
-		*/
+		
+		--------------------------------------------------------------------
+		
 		CatalogoLivros catalogo = new CatalogoLivros();
 		catalogo.adicionarLivro("Livro 1", "Autor 1", 2021);
 		catalogo.adicionarLivro("Livro 2", "Autor 2", 2022);
@@ -46,6 +49,16 @@ public class Main {
 		System.out.println(catalogo.pesquisarPorAutor("Autor 4"));
 		System.out.println(catalogo.pesquisarPorIntervaloAnos(2020, 2021));
 		System.out.println(catalogo.pesquisarPorTitulo("Livro 3"));
+		*/
+		OrdenacaoPessoa ord = new OrdenacaoPessoa();
+		ord.adicionarPessoa("Victor", 20, 1.68);
+		ord.adicionarPessoa("Clara", 12, 1.50);
+		ord.adicionarPessoa("Larissa", 23, 1.60);
+		ord.adicionarPessoa("Leticia", 21, 1.65);
+		ord.adicionarPessoa("Cris", 20, 1.80);
+		ord.adicionarPessoa("Paulo", 21, 1.72);
+		
+		System.out.println(ord.ordenarPorAltura());
 	}
 
 }
---
+++ src/list/Ordenacao/Pessoa.java
@@ -0,0 +1,59 @@
+package list.Ordenacao;
+
+import java.util.Comparator;
+
+public class Pessoa implements Comparable<Pessoa> {
+	private String nome;
+	private int idade;
+	private double altura;
+	public Pessoa(String nome, int idade, double altura) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.altura = altura;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	@Override
+	public String toString() {
+		return "\n [nome=" + nome + ", idade=" + idade + ", altura=" + altura + "]";
+	}
+
+	@Override
+	public int compareTo(Pessoa p) {
+		return Integer.compare(idade, p.getIdade());
+	}
+	
+	
+}
+class ComparatorPorAltura implements Comparator<Pessoa>{
+
+	@Override
+	public int compare(Pessoa p1, Pessoa p2) {
+		return Double.compare(p1.getAltura(), p2.getAltura());
+	}
+	
+}
