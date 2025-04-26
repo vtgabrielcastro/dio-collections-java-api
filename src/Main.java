ada4a1650bfd14d6f0f77b35f78406d3a8c8f322
Victor <victorgabrielcastrosilva2015@gmail.com>
2025-04-26 19:59:58 -0300

    Pesquisas com List

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-import list.OperacoesBasic.CarrinhoDeCompras;
+import list.Pesquisa.CatalogoLivros;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*ListaTarefa listaTarefa = new ListaTarefa();
+		
+		/*
+		 * ListaTarefa listaTarefa = new ListaTarefa();
 		System.out.println("O numero total de elementos é: " + listaTarefa.obterNumeroTotalTarefas());
 		
 		listaTarefa.adicionarTarefa("Tarefa 1");
@@ -16,8 +17,8 @@ public class Main {
 		//System.out.println("O numero total de elementos é: " + listaTarefa.obterNumeroTotalTarefas());
 		
 		listaTarefa.obterDescricoesTarefas();
-		*/
 		
+		-------------------------------------------------------
 		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
 		
 		carrinho.adicionarItem("Camisa", 20, 1);
@@ -35,8 +36,16 @@ public class Main {
 		carrinho.exibirItens();
 		
 		System.out.println("Valor total: " + carrinho.calcularValorTotal());
+		*/
+		CatalogoLivros catalogo = new CatalogoLivros();
+		catalogo.adicionarLivro("Livro 1", "Autor 1", 2021);
+		catalogo.adicionarLivro("Livro 2", "Autor 2", 2022);
+		catalogo.adicionarLivro("Livro 3", "Autor 3", 2023);
+		catalogo.adicionarLivro("Livro 4", "Autor 4", 2024);
 		
-		
+		System.out.println(catalogo.pesquisarPorAutor("Autor 4"));
+		System.out.println(catalogo.pesquisarPorIntervaloAnos(2020, 2021));
+		System.out.println(catalogo.pesquisarPorTitulo("Livro 3"));
 	}
 
 }
